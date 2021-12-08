50dca6c76c130f6d5e3db0e1b98f90d0312b4734
harikiran01 <hari.kiran@pocketfm.in>
2021-12-09 03:02:58 +0530

    replace ips

--- app/src/main/java/com/hk/ijournal/common/CommonLib.kt
+++ app/src/main/java/com/hk/ijournal/common/CommonLib.kt
@@ -2,4 +2,9 @@ package com.hk.ijournal.common
 
 object CommonLib {
     const val LOGTAG = "LeakIssue"
+    const val API_QA = "13.229.188.140/v2"
+
+    //const val API_QA = "52.221.203.54/v2"
+    //const val API_QA = "18.142.49.30/v2"
+    //const val API_QA = "52.221.184.13/v2"
 }
